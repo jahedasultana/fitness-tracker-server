ceb5900cca2cf8c03e35b986d88e88350b16ff44
jaheda8256 <jahedasultana47@gmail.com>
2024-06-04 02:19:11 +0600

    added get a single data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+// const jwt = require("jsonwebtoken");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wotzmkf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,16 +22,68 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+   
+    const classesCollection = client.db("fitnessDb").collection("classes");
+    const trainerCollection = client.db("fitnessDb").collection("trainers");
+
+
+    //  // jwt related api
+    //  app.post("/jwt", async (req, res) => {
+    //   const user = req.body;
+    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    //     expiresIn: "1h",
+    //   });
+    //   res.send({ token });
+    // });
+
+    // middlewares
+    // const verifyToken = (req, res, next) => {
+    //   console.log("inside verify token", req.headers.authorization);
+    //   if (!req.headers.authorization) {
+    //     return res.status(401).send({ message: "unauthorized access" });
+    //   }
+    //   const token = req.headers.authorization.split(" ")[1];
+    //   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    //     if (err) {
+    //       return res.status(401).send({ message: "unauthorized access" });
+    //     }
+    //     req.decoded = decoded;
+    //     next();
+    //   });
+    // };
+
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
+   
+
+
+    // get all classes from db
+    app.get('/classes', async(req, res) =>{
+      const result = await classesCollection.find().toArray()
+      res.send(result);
+    });
+
+
+        // get all trainers from db
+    app.get('/trainers', async(req, res) =>{
+      const result = await trainerCollection.find().toArray()
+      res.send(result);
+    });
+
+        // get a single data all from db using id
+    app.get('/trainer/:id', async(req, res) =>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await trainerCollection.findOne(query)
+      res.send(result);
+    });
+
+
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    
   }
 }
 run().catch(console.dir);
