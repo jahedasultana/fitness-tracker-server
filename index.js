b6cc13b9d65b68992c747c934ade69e93c796345
jaheda8256 <jahedasultana47@gmail.com>
2024-06-16 08:22:33 +0600

    added new post and delete

--- index.js
+++ index.js
@@ -27,19 +27,20 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const classesCollection = client.db("fitnessDb").collection("classes");
-    const trainerCollection = client.db("fitnessDb").collection("trainers");
-    const usersCollection = client.db("fitnessDb").collection("users");
-    const trainerApplicationCollection = client
-      .db("fitnessDb")
-      .collection("trainerApplications");
+ 
 
-    const subscriptionsCollection = client
-      .db("fitnessDb")
-      .collection("subscriptions");
+  const classCollection = client.db('fitnessDb').collection('class');
+  const trainerCollection = client.db('fitnessDb').collection('trainers');
+  const slotsCollection = client.db('fitnessDb').collection('slots');
 
-      const trainerSlotsCollection = client.db("fitnessDb").collection("trainerSlots");
-      
+  const forumCollection = client.db('fitnessDb').collection('forum');
+  const reviewCollection = client.db('fitnessDb').collection('review');
+
+  const usersCollection = client.db('fitnessDb').collection('users');
+  const paymentCollection = client.db('fitnessDb').collection('payment');
+
+  const subscribeCollection = client.db('fitnessDb').collection('subscribers');
+
 
     // jwt related api
     app.post("/jwt", async (req, res) => {
@@ -68,124 +69,260 @@ async function run() {
 
     // Send a ping to confirm a successful connection
 
-    // // save a user data in db
-    app.put("/user", async (req, res) => {
-      const user = req.body;
-      const query = { email: user?.email };
-      //check if user already exists in db
-      const isExist = await usersCollection.findOne(query);
-      if (isExist) return res.send(isExist);
+     app.get('/slots', async (req, res) => {
+            const result = await slotsCollection.find({
+                status: 'pending'
+            }).toArray();
+            res.send(result);
+        })
+        // save become trainer slots data to db
+        app.post('/slots', async (req, res) => {
+            const slot = req.body;
+            const result = await slotsCollection.insertOne(slot)
+            res.send(result);
+        })
+        //Trainer slots 
+        app.post('/trainers', async (req, res) => {
+            const slot = req.body;
+            const result = await trainerCollection.insertOne(slot)
+            res.send(result);
+        })
+        // get slot data form db
+        app.get("/slot/:email", async (req, res) => {
+            try {
+                const email = req.params.email;
+                const query = { email: email }
+                const result = await slotsCollection.findOne(query);
+                res.send(result);
+            } catch (error) {
+                res.status(500).send({ message: "Failed to fetch trainer slots data", error });
+            }
+        });
 
-      // save user for the first time
-      const options = { upsert: true };
-      const updateDoc = {
-        $set: {
-          ...user,
-          timestamp: Date.now(),
-        },
-      };
-      const result = await usersCollection.updateOne(query, updateDoc, options);
-      res.send(result);
-    });
+        app.post("/slot/make-trainer/:email", async (req, res) => {
+            try {
+                if (req.body.make_trainer) {
+                    const email = req.params.email;
+                    const query = { email: email }
+                    const result = await slotsCollection.updateOne(query, {
+                        $set: { status: "approved" },
+                    });
+                    // user role to trainer
+                    const trainerQuery = { email: email }
+                    const trainerResult = await trainerCollection.updateOne(trainerQuery, {
+                        $set: { role: "trainer" },
+                    });
 
-    // get a user info by email from db
-    app.get("/user/:email", async (req, res) => {
-      const email = req.params.email;
-      const result = await usersCollection.findOne({ email });
-      res.send(result);
-    });
+                    res.send(result);
+                } else {
 
-    // get all users data from db
-    app.get("/users", verifyToken, async (req, res) => {
-      const result = await usersCollection.find().toArray();
-      res.send(result);
-    });
-
-    // get all classes from db
-    app.get("/classes", async (req, res) => {
-      const result = await classesCollection.find().toArray();
-      res.send(result);
-    });
-
-    // Add new class
-    app.post("/classes", async (req, res) => {
-      const classData = req.body;
-      console.log(classData);
-      const result = await classesCollection.insertOne(classData);
-      res.send(result);
-    });
-
-    // get all trainers from db
-    app.get("/trainers", async (req, res) => {
-      const result = await trainerCollection.find().toArray();
-      res.send(result);
-    });
-
-    // get a single data all from db using id
-    app.get("/trainer/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await trainerCollection.findOne(query);
-      res.send(result);
-    });
-
-    // Add the new route here
-    app.post("/trainerApplications", verifyToken, async (req, res) => {
-      const trainerData = req.body;
-      try {
-        const result = await trainerApplicationCollection.insertOne(
-          trainerData
-        );
-        res.send(result);
-      } catch (error) {
-        res.status(500).send({ message: "Failed to save trainer data", error });
-      }
-    });
-
-    // Handle subscription POST request
-    app.post("/subscribe", async (req, res) => {
-      const subscriptionData = req.body;
-      try {
-        const result = await subscriptionsCollection.insertOne(
-          subscriptionData
-        );
-        res.send(result);
-      } catch (error) {
-        res
-          .status(500)
-          .send({ message: "Failed to save subscription data", error });
-      }
-    });
-
-    // Endpoint to get all subscribers
-    app.get("/subscribers", async (req, res) => {
-      try {
-        const result = await subscriptionsCollection.find().toArray();
-        res.send(result);
-      } catch (error) {
-        res
-          .status(500)
-          .send({ message: "Failed to fetch subscribers data", error });
-      }
-    });
-
-
-    // Add trainer slot
-    app.post("/trainerSlots", verifyToken, async (req, res) => {
-      const slotData = req.body;
-      try {
-        const result = await trainerSlotsCollection.insertOne(slotData);
-        res.send(result);
-      } catch (error) {
-        res.status(500).send({ message: "Failed to save slot data", error });
-      }
-    });
+                }
 
 
 
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+            } catch (error) {
+                res.status(500).send({ message: "Failed to fetch trainer slots data", error });
+            }
+        });
+        //slot data delete 
+        app.delete('/slot/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const result = await slotsCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        app.post('/users/trainer/demote/:email', async (req, res) => {
+            const trainer = await trainerCollection.findOne({
+                email: req.params.email
+            });
+            if (!trainer) {
+                return res.status(404).send({ error: 'Trainer not found' });
+            }
+            const result = await usersCollection.updateOne({
+                email: req.params.email
+            }, {
+                $set: { role: 'member' },
+            })
+            res.json({
+                success: true,
+                message: 'demoted successfully'
+            }).status(200)
+        })
+        //save user data in database
+        app.put('/user', async (req, res) => {
+            const user = req.body;
+            const query = { email: user?.email }
+            // check if user already in database
+            const isExist = await usersCollection.findOne(query);
+            if (isExist) {
+                if (user.status === 'Requested') {
+                    // if existing user try to change his role
+                    const result = await usersCollection.updateOne(query, {
+                        $set: { status: user?.status },
+                    })
+                    return res.send(result)
+                } else {
+                    // if existing user login again
+                    return res.send(isExist)
+                }
+            }
+            // save user for the first time
+            const options = { upsert: true }
+            const updateDoc = {
+                ...user,
+                Timestamp: Date.now()
+
+            }
+            const result = await usersCollection.insertOne(updateDoc, options)
+            res.send(result)
+        })
+        // get all user email from db.
+        app.get('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const result = await usersCollection.findOne({ email });
+            res.send(result);
+        })
+        // get all users data from db
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result)
+        })
+        app.get('/users/trainer', async (req, res) => {
+            const result = await usersCollection.find({
+                role: 'trainer'
+            }).toArray()
+            res.send(result)
+        })
+        // Forum API 
+        app.get('/forum', async (req, res) => {
+            const result = await forumCollection.find().toArray();
+            res.send(result);
+        })
+        //review related Api
+        app.get('/review', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        })
+        // Save subscribe data in DB
+        app.post("/subscribe", async (req, res) => {
+            const subscriptionData = req.body;
+            try {
+                const result = await subscribeCollection.insertOne(subscriptionData);
+                res.send(result);
+            } catch (error) {
+                res.status(500).send({ message: "Failed to save subscription data", error });
+            }
+        });
+
+        // get all all subscribers data from DB
+        app.get("/subscribers", async (req, res) => {
+            try {
+                const result = await subscribeCollection.find().toArray();
+                res.send(result);
+            } catch (error) {
+                res.status(500).send({ message: "Failed to fetch subscribers data", error });
+            }
+        });
+
+        // collect all class from database
+        app.get('/classes', async (req, res) => {
+            const result = await classCollection.find().toArray();
+            res.send(result)
+        })
+        // save add class data from database 
+        app.post("/class", async (req, res) => {
+            const ClassData = req.body;
+            const result = await classCollection.insertOne(ClassData);
+            res.send(result);
+        });
+        app.post("/pay-now", async (req, res) => {
+            const { trainer_info, trainer_id, slot_name, package_name, price, user_id, email, otherInfo } = req.body;
+
+            // Validate data (add more validation as needed)
+            if (!trainer_info || !trainer_id || !slot_name || !package_name || !price || !email) {
+                return res.status(422).json({ error: 'Missing required fields' });
+            }
+
+            // Create a new payment document
+            const newPayment = {
+                trainer_info,
+                trainer_id,
+                slot_name,
+                package_name,
+                price,
+                user_id,
+                email,
+                created_at: new Date(),
+            };
+
+            // Insert the payment document into the collection
+            const result = await paymentCollection.insertOne(newPayment);
+
+            res.json({
+                'message': "Payment successful",
+                'data': result,
+                'result': true
+            }).status(200);
+        });
+        app.get("/payment-list", async (req, res) => {
+
+            // Insert the payment document into the collection
+            const result = await paymentCollection.find().sort({
+                crated_at: 1
+            }).toArray();
+
+            res.json({
+                'data': result,
+                'result': true
+            }).status(200);
+        });
+        // collect single class data from database
+        app.get('/class/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await classCollection.findOne(query);
+            res.send(result)
+        })
+        // 
+        app.get('/trainers/class/:id', async (req, res) => {
+            id = req.params.id;
+            const query = { classes: id }
+            const result = await trainerCollection.find(query).toArray();
+            res.send(result)
+        })
+        // collect all trainer data from database
+        app.get('/trainers', async (req, res) => {
+            const result = await trainerCollection.find().toArray();
+            res.send(result)
+        })
+        // collect trainer details  data from database
+        app.get('/trainer/:id', async (req, res) => {
+            id = req.params.id;
+            if (!ObjectId.isValid(id)) {
+                return res.status(400).send({ error: 'Invalid trainer ID format' });
+            }
+            const query = { _id: new ObjectId(id) }
+            const result = await trainerCollection.findOne(query);
+            if (!result) {
+                return res.status(404).send({ error: 'Trainer not found' });
+            }
+            let classList = [];
+            if (result.classes && Array.isArray(result.classes)) {
+                classList = await Promise.all(
+                    result.classes.map(async (slot) => {
+                        return await classCollection.findOne({ _id: new ObjectId(slot) });
+                    })
+                );
+            }
+
+            result.classLists = classList;
+            res.send(result);
+        })
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
   }
 }
